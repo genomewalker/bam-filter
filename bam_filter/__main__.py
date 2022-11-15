83980c6c40e7f1cc5f643abc42094525bfbb00a0
genomewalker <genomewalker@gmail.com>
2022-11-15 01:14:04 +0100

    Minor

--- bam_filter/__main__.py
+++ bam_filter/__main__.py
@@ -93,6 +93,7 @@ def main():
         read_length_freqs=args.read_length_freqs,
     )
     logging.info("Reducing results to a single dataframe")
+    data = list(filter(None, data))
     data_df = [x[0] for x in data]
     # data_df = fast_flatten(list(filter(None, data_df)))
     data_df = concat_df(data_df)
