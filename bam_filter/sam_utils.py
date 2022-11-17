c1290cfaca4958d98801c6b0c52fba0be67fd68a
genomewalker <genomewalker@gmail.com>
2022-11-17 20:47:49 +0100

    Test

--- bam_filter/sam_utils.py
+++ bam_filter/sam_utils.py
@@ -143,6 +143,9 @@ def get_bam_stats(
             contig=reference, multiple_iterators=False, until_eof=False
         ):
             ani_read = (1 - ((aln.get_tag("NM") / aln.infer_query_length()))) * 100
+            if aln.query_name == "M_A00706:52:HJYNKDSXX:1:1101:30246:1485":
+                print("ani_read")
+                exit()
             if ani_read >= min_read_ani:
                 n_alns += 1
                 read_hits[aln.query_name] += 1
