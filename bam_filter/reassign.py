Fixed integer overflow for billion-sized bam files

bam_filter/reassign.py
@@ -39,7 +39,7 @@ def initialize_subject_weights(data):
         data["s_W"] = 1 / data["slen"]
 
         # Calculate the sum of weights for each unique source
-        sum_weights = np.zeros(int(np.max(data["source"])) + 1)
+        sum_weights = np.zeros(np.int64(np.max(data["source"])) + 1)
         np.add.at(sum_weights, data["source"], data["var"])
 
         # Calculate the normalized weights based on the sum for each query
@@ -52,14 +52,13 @@ def initialize_subject_weights(data):
 
 
 def resolve_multimaps(data, scale=0.9, iters=10):
-
     current_iter = 0
     while True:
         progress_bar = tqdm.tqdm(
             total=9,
             desc=f"Iter {current_iter + 1}",
             unit=" step",
-            disable=False,
+            disable=False,  # Replace with your logic or a boolean value
             leave=False,
             ncols=80,
         )
@@ -71,7 +70,7 @@ def resolve_multimaps(data, scale=0.9, iters=10):
         # Calculate the weights for each subject
         log.debug(f"::: Iter: {current_iter + 1} - Calculating weights...")
         progress_bar.update(1)
-        subject_weights = np.zeros(int(np.max(data["subject"])) + 1)
+        subject_weights = np.zeros(np.int64(np.max(data["subject"])) + 1)
         np.add.at(subject_weights, data["subject"], data["prob"])
         data["s_W"] = subject_weights[data["subject"]] / data["slen"]
         subject_weights = None
@@ -83,7 +82,7 @@ def resolve_multimaps(data, scale=0.9, iters=10):
         log.debug("Calculating sum of probabilities")
         progress_bar.update(1)
         prob_sum = data["prob"] * data["s_W"]
-        prob_sum_array = np.zeros(int(np.max(data["source"])) + 1)
+        prob_sum_array = np.zeros(np.int64(np.max(data["source"])) + 1)
         np.add.at(prob_sum_array, data["source"], prob_sum)
         prob_sum = None
 
@@ -94,7 +93,7 @@ def resolve_multimaps(data, scale=0.9, iters=10):
         log.debug("Calculating query counts")
         progress_bar.update(1)
         # Calculate how many alignments are in each query
-        # query_counts = np.zeros(int(np.max(data["source"])) + 1)
+        # query_counts = np.zeros(np.int64(np.max(data["source"])) + 1)
         # np.add.at(query_counts, data["source"], 1)
 
         query_counts = np.bincount(data["source"])
@@ -102,7 +101,7 @@ def resolve_multimaps(data, scale=0.9, iters=10):
         log.debug("Calculating query counts array")
         progress_bar.update(1)
         # Use a separate array for query counts
-        query_counts_array = np.zeros(int(np.max(data["source"])) + 1)
+        query_counts_array = np.zeros(np.int64(np.max(data["source"])) + 1)
         np.add.at(
             query_counts_array,
             data["source"],
@@ -124,7 +123,6 @@ def resolve_multimaps(data, scale=0.9, iters=10):
             progress_bar.close()
             log.info("::: ::: No more multimapping reads. Early stopping.")
             return data
-
         data = data[(data["n_aln"] > 1) & (data["prob"] > 0)]
 
         # total_n_unique = np.sum(query_counts_array[data["source"]] <= 1)
@@ -134,7 +132,7 @@ def resolve_multimaps(data, scale=0.9, iters=10):
 
         log.debug("Calculating max_prob")
         # Keep the ones that have a probability higher than the maximum scaled probability
-        max_prob = np.zeros(int(np.max(data["source"])) + 1)
+        max_prob = np.zeros(np.int64(np.max(data["source"])) + 1)
         np.maximum.at(max_prob, data["source"], data["prob"])
 
         data["max_prob"] = max_prob[data["source"]]
@@ -145,8 +143,6 @@ def resolve_multimaps(data, scale=0.9, iters=10):
         )
         progress_bar.update(1)
         to_remove = np.sum(data["prob"] < data["max_prob"])
-
-        # data_to_remove = data[data["prob"] < data["max_prob"]]
 
         data = data[data["prob"] >= data["max_prob"]]
         max_prob = None
@@ -319,7 +315,11 @@ def write_reassigned_bam(
     log.info("::: Creating reference chunks with uniform read amounts...")
 
     ref_chunks = sort_keys_by_approx_weight(
-        input_dict=ref_counts, scale=1, num_cores=num_cores, verbose=False
+        input_dict=ref_counts,
+        scale=1,
+        num_cores=num_cores,
+        verbose=False,
+        max_entries_per_chunk=1_000_000,
     )
 
     num_cores = min(num_cores, len(ref_chunks))
@@ -415,14 +415,14 @@ def write_reassigned_bam(
             s_threads = threads
         samfile = pysam.AlignmentFile(out_bam, "rb", threads=s_threads)
         chr_lengths = []
-        for chrom in samfile.references:
-            chr_lengths.append(samfile.get_reference_length(chrom))
-        max_chr_length = np.max(chr_lengths)
+        # for chrom in samfile.references:
+        #     chr_lengths.append(samfile.get_reference_length(chrom))
+        # max_chr_length = np.max(chr_lengths)
         pysam.set_verbosity(save)
         samfile.close()
 
-        if max_chr_length > 536870912:
-            logging.info("A reference is longer than 2^29")
+        # if max_chr_length > 536870912:
+        #     logging.info("A reference is longer than 2^29")
         pysam.index(
             "-c",
             "-@",
@@ -500,9 +500,9 @@ def write_reassigned_bam(
 
 #     for reference in references:
 #         if ref_lengths is None:
-#             reference_length = int(samfile.get_reference_length(reference))
+#             reference_length = np.int64(samfile.get_reference_length(reference))
 #         else:
-#             reference_length = int(ref_lengths[reference])
+#             reference_length = np.int64(ref_lengths[reference])
 #         aln_data = []
 #         for aln in samfile.fetch(
 #             contig=reference, multiple_iterators=False, until_eof=True
@@ -608,9 +608,9 @@ def get_bam_data(
 
     for reference in references:
         reference_length = (
-            int(samfile.get_reference_length(reference))
+            np.int64(samfile.get_reference_length(reference))
             if ref_lengths is None
-            else int(ref_lengths[reference])
+            else np.int64(ref_lengths[reference])
         )
         aln_data = []
         for aln in samfile.fetch(
@@ -731,10 +731,21 @@ def reassign_reads(
     # Filter out references with less than min_read_count reads
     # add a progress bar
 
+    # references_m = {
+    #     chrom.contig: chrom.mapped
+    #     for chrom in index_statistics
+    #     if chrom.mapped >= min_read_count
+    # }
     references_m = {
         chrom.contig: chrom.mapped
-        for chrom in index_statistics
-        if chrom.mapped >= min_read_count
+        for chrom in tqdm.tqdm(
+            [chrom for chrom in index_statistics if chrom.mapped >= min_read_count],
+            desc="Filtering Chromosomes",
+            total=len(index_statistics),
+            unit="chrom",
+            leave=False,
+            ncols=80,
+        )
     }
     # get number alignments
     n_alns = sum(references_m.values())
@@ -862,8 +873,8 @@ def reassign_reads(
 
             # Create chunks of 1 billion rows each
             for chunk_idx in range(num_chunks):
-                start = int(chunk_idx * 1e9)
-                end = int(min((chunk_idx + 1) * 1e9, data[i][0].nrows))
+                start = np.int64(chunk_idx * 1e9)
+                end = np.int64(min((chunk_idx + 1) * 1e9, data[i][0].nrows))
                 chunks.append(data[i][0][start:end, :])
 
             # Substitute data[i] with the first chunk
@@ -944,7 +955,7 @@ def reassign_reads(
 
     # Preallocate the NumPy array
     mat = np.empty(
-        (total_rows, num_columns), dtype=np.float32
+        (total_rows, num_columns), dtype=np.float64
     )  # Adjust dtype as necessary
 
     current_index = 0
@@ -1032,18 +1043,18 @@ def reassign_reads(
     # Define the dtype for the structured array
     dtype = np.dtype(
         [
-            ("source", "int"),
-            ("subject", "int"),
-            ("var", "float"),
-            ("slen", "int"),
+            ("source", "int64"),
+            ("subject", "int64"),
+            ("var", "float64"),
+            ("slen", "int64"),
             (
                 "s_W",
                 "float",
             ),  # This and following fields are initialized to 0 or a default value
-            ("prob", "float"),
-            ("iter", "int"),
-            ("n_aln", "int"),
-            ("max_prob", "float"),
+            ("prob", "float64"),
+            ("iter", "int64"),
+            ("n_aln", "int64"),
+            ("max_prob", "float64"),
         ]
     )
 

bam_filter/sam_utils.py
@@ -150,15 +150,15 @@ def write_bam(bam, references, output_files, threads=1, sort_memory="1G"):
     samfile = pysam.AlignmentFile(
         output_files["bam_tmp_sorted"], "rb", threads=s_threads
     )
-    chr_lengths = []
-    for chrom in samfile.references:
-        chr_lengths.append(samfile.get_reference_length(chrom))
-    max_chr_length = np.max(chr_lengths)
+    # chr_lengths = []
+    # for chrom in samfile.references:
+    #     chr_lengths.append(samfile.get_reference_length(chrom))
+    # max_chr_length = np.max(chr_lengths)
     pysam.set_verbosity(save)
     samfile.close()
 
-    if max_chr_length > 536870912:
-        logging.info("::: ::: A reference is longer than 2^29")
+    # if max_chr_length > 536870912:
+    #     logging.info("::: ::: A reference is longer than 2^29")
     pysam.index(
         "-c",
         "-@",
@@ -382,7 +382,7 @@ def get_bam_stats(
 
             max_covered_bases = np.max(ranges)
             mean_covered_bases = np.mean(ranges)
-            bases_covered = int(len(cov_pos))
+            bases_covered = np.int64(len(cov_pos))
             # get SD from covered bases
             cov_sd = np.std(cov_pos, ddof=1)
             cov_var = np.var(cov_pos, ddof=1)
@@ -775,18 +775,15 @@ def check_bam_file(
 
     # Use a with statement to ensure proper closing of the samfile
     try:
-        if threads > 4:
-            s_threads = 4
-        else:
-            s_threads = threads
+        s_threads = min(threads, 4)
 
         with pysam.AlignmentFile(bam, "rb", threads=s_threads) as samfile:
             references = samfile.references
-
-            chr_lengths = []
-            for chrom in samfile.references:
-                chr_lengths.append(samfile.get_reference_length(chrom))
-            max_chr_length = np.max(chr_lengths)
+            log.info(f"Found {samfile.nreferences:,} reference sequences")
+            # log.info("Getting reference lengths...")
+            references = samfile.references
+            # chr_lengths = [samfile.get_reference_length(chrom) for chrom in references]
+            # max_chr_length = np.max(chr_lengths)
 
             pysam.set_verbosity(save)
             ref_lengths = None
@@ -804,31 +801,24 @@ def check_bam_file(
                         "The BAM file contains references not found in the reference lengths file"
                     )
                     sys.exit(1)
-                max_chr_length = np.max(ref_lengths["length"].tolist())
+                # max_chr_length = np.max(ref_lengths["length"].tolist())
 
             # Check if BAM files is not sorted by coordinates, sort it by coordinates
-            if not samfile.header["HD"]["SO"] == "coordinate":
+            if samfile.header["HD"]["SO"] != "coordinate":
                 log.info("BAM file is not sorted by coordinates, sorting it...")
                 sorted_bam = bam.replace(".bam", ".bf-sorted.bam")
-                if threads > 4:
-                    s_threads = 4
-                else:
-                    s_threads = threads
                 pysam.sort(
                     "-@", str(s_threads), "-m", str(sort_memory), "-o", sorted_bam, bam
                 )
                 bam = sorted_bam
                 pysam.index("-c", "-@", str(threads), bam)
-                if threads > 4:
-                    s_threads = 4
-                else:
-                    s_threads = threads
+
                 samfile = pysam.AlignmentFile(bam, "rb", threads=s_threads)
 
             if not samfile.has_index():
                 logging.info("BAM index not found. Indexing...")
-                if max_chr_length > 536870912:
-                    logging.info("A reference is longer than 2^29")
+                # if max_chr_length > 536870912:
+                #     logging.info("A reference is longer than 2^29")
                 pysam.index("-c", "-@", str(threads), bam)
 
             logging.info("::: BAM file looks good.")
@@ -960,7 +950,11 @@ def process_bam(
 
     # ify the number of chunks
     ref_chunks = sort_keys_by_approx_weight(
-        input_dict=references_m, scale=1, num_cores=threads, verbose=False
+        input_dict=references_m,
+        scale=1,
+        num_cores=threads,
+        verbose=False,
+        max_entries_per_chunk=25_000_000,
     )
     log.info(f"::: Created {len(ref_chunks):,} chunks")
     ref_chunks = random.sample(ref_chunks, len(ref_chunks))
@@ -1220,7 +1214,11 @@ def filter_reference_BAM(
             # batch_size = len(references) // num_cores + 1  # Ensure non-zero batch size
             log.info("::: Creating reference chunks with uniform read amounts...")
             ref_chunks = sort_keys_by_approx_weight(
-                input_dict=ref_dict_m, scale=1, num_cores=threads, verbose=False
+                input_dict=ref_dict_m,
+                scale=1,
+                num_cores=threads,
+                verbose=False,
+                max_entries_per_chunk=1_000_000,
             )
             num_cores = min(num_cores, len(ref_chunks))
             log.info(f"::: Using {num_cores} cores to write {len(ref_chunks)} chunk(s)")
@@ -1318,15 +1316,15 @@ def filter_reference_BAM(
                     samfile = pysam.AlignmentFile(
                         out_files["bam_filtered"], "rb", threads=s_threads
                     )
-                    chr_lengths = []
-                    for chrom in samfile.references:
-                        chr_lengths.append(samfile.get_reference_length(chrom))
-                    max_chr_length = np.max(chr_lengths)
+                    # chr_lengths = []
+                    # for chrom in samfile.references:
+                    #     chr_lengths.append(samfile.get_reference_length(chrom))
+                    # max_chr_length = np.max(chr_lengths)
                     pysam.set_verbosity(save)
                     samfile.close()
 
-                    if max_chr_length > 536870912:
-                        logging.info("A reference is longer than 2^29")
+                    # if max_chr_length > 536870912:
+                    #     logging.info("A reference is longer than 2^29")
                     pysam.index(
                         "-c",
                         "-@",